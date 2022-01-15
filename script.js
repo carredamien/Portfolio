b5f1a64e2c22998ecf9ae5525d832f3c2b665b6f
Damien Carré <damiencarre07@gmail.com>
2022-01-15 14:39:14 +0100

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('.fa-chevron-up');
 
 btn.addEventListener('click', () => {
   window.scrollTo({
-    top:0,
+    top:100,
     left:0,
     behavior:"smooth"
   })
