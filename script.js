d905a71d9783827f3305df7346a6c2509642b97e
Damien Carré <damiencarre07@gmail.com>
2022-02-23 13:23:47 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,96 @@
-const btn = document.querySelector('.fa-chevron-up');
+/**
+ * Menu mobile
+ * button burger for mobile and tablet
+ */
+const burger = document.querySelector(".burger");
+const navMobil = document.querySelector(".nav--mobil");
+const body = document.querySelector(".body");
+const lis = document.querySelectorAll(".navbar li");
 
-btn.addEventListener('click', () => {
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  navMobil.classList.toggle("nav--open");
+  body.classList.toggle("overflow");
+});
+
+lis.forEach(li => {
+  li.addEventListener("click" , () => {
+    navMobil.classList.toggle("nav--open");
+    burger.classList.toggle("active");
+    body.classList.toggle("overflow");
+  });
+});
+/**
+ * Animations 
+ */
+const h1 = document.querySelector(".text--h1");
+
+new Typewriter(h1, {
+  loop: true,
+})
+  .changeDelay(110)
+  // // .typeString("Besoin d'un site ")
+  .typeString("Je suis développeur web")
+  .pauseFor(500)
+  .deleteChars(23)
+  .typeString("Je travaille avec javascript")
+  .pauseFor(400)
+  .deleteChars(10)
+  .typeString("VueJs et NodeJs")
+  .pauseFor(400)
+  .deleteChars(33)
+  .typeString("mais aussi avec WordPress et PhP.")
+  .pauseFor(3000)
+  .deleteChars(17)
+  .start();
+
+/**
+ * button up
+ */
+const btn = document.querySelector("#up");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction(){
+  if (window.pageYOffset > 600){
+    btn.style.right = -1.5 + "rem";
+  }else{
+    btn.style.right = -6.5 + "rem";
+  }
+}
+btn.addEventListener("click", () => {
   window.scrollTo({
-    top:100,
+    top:0,
     left:0,
     behavior:"smooth"
-  })
+  });
 });
+
+/**
+ * Show Year of copyright
+ */
+const footerCopyright = document.querySelector('.footer__content--p a');
+const currentYear = new Date().getFullYear();
+
+function copyright () {
+  footerCopyright.textContent = `© ${ currentYear } - Tout droits réservés: Damien Carré`;
+}
+copyright();
+
+// Disable
+function disableselect(e){ 
+return false; 
+} 
+
+function reEnable(){ 
+return true; 
+} 
+
+//if IE4+ 
+document.onselectstart = new Function ("return false"); 
+document.oncontextmenu = new Function ("return false"); 
+//if NS6 
+if (window.sidebar){ 
+document.onmousedown=disableselect;
+document.onclick=reEnable ;
+} 
